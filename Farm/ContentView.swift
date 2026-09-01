fb73d82cbe72e29d85ca4898aba2e9384344c5c6
Keya Aggarwal <ka546@cornell.edu>
2026-09-01 17:39:50 -0400

    Keya's farm intro, coding without vibe coding the whole thinglol

--- Farm/ContentView.swift
+++ Farm/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var happiness = 50
+    @State private var duckIsGrowing = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("My Little World")
+            if happiness < 51 {
+                Text("🥺")
+            } else if happiness <= 70 {
+                Text("🐥")
+                    .scaleEffect(duckIsGrowing ? CGFloat(1.3) : CGFloat(1.0))
+                    .animation(.spring(), value:duckIsGrowing)
+            } else {
+                Text("😄")
+            }
+            Text("Happiness: \(happiness)")
+            Button("feed me"){
+                duckIsGrowing = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {duckIsGrowing = false}
+                happiness = happiness+10
+            }
+            
         }
         .padding()
     }
