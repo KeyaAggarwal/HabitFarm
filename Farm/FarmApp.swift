3f49d894417538f69b6420528d485a58f451298b
Keya Aggarwal <ka546@cornell.edu>
2026-09-01 17:05:03 -0400

    Initial Commit

---
+++ Farm/FarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmApp.swift
+//  Farm
+//
+//  Created by Keya Aggarwal on 01/09/26.
+//
+
+import SwiftUI
+
+@main
+struct FarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FarmTests/FarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  FarmTests.swift
+//  FarmTests
+//
+//  Created by Keya Aggarwal on 01/09/26.
+//
+
+import Testing
+@testable import Farm
+
+struct FarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
